6cf03f8c325b77224ea5ea3055035c4b13163936
Murd4 <hectorpadillaizquierdo@gmail.com>
2017-10-27 13:36:17 +0200

    Folder creation optimized using IFS
    (Internal Field Separator)

--- tree.sh
+++ tree.sh
@@ -3,20 +3,33 @@ echo "
 **********************
 Command-Line Assesment:
 
-1. Type 'tree' to make a folder tree.
+1. Type 'tree' to create a folder tree.
 2. Once created, type 'find' to find a file inside it.
 "
 read -p "Introduce your option: " opt1
 if [[ $opt1 == "tree" ]]; then
-	mkdir -p 1/2/3
-	touch 1/2/3/foo
-	echo " -> Folder tree created."
-	read -p "Do you want to display the tree? [y/n] ('tree' must be installed.)" opt2
+	read -p "Introduce the number of folders" folder_num
+	a=0
+	b=1
+	while [ $a -lt $folder_num ]
+	do
+		read -p "Introduce the name of folder ${b}" folder[$a]
+		((a+=1))
+		((b+=1))
+	done
+	IFS=/
+	mkdir -p "${folder[*]})"
+	touch "${folder[*]})/foo"
+	echo -e " -> Folder tree created"
+	read -p "Do you want to display the tree? [y/n] (It might not work if 'tree' is not installed.)" opt2
 	if [[ $opt2 == "y"  ]]; then
-		tree 1
+		tree ${folder[0]}
 	fi
 elif [[ $opt1 == "find" ]]; then
 	read -p "Introduce a search term (i.e foo) : " word1
-	find ./1 -name $word1
+	find . -name $word1
 fi 
+
+
+
 
